760765f25192626c1342f116cb497f7639f1ce2e
davidebbo <david.ebbo@microsoft.com>
2014-03-01 21:49:05 -0800

    Initial commit

---
+++ OrganizePhotos/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExifLib;
+
+namespace OrganizePhotos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string folder = args.Length == 0 ? "." : args[0];
+
+            foreach (string file in Directory.EnumerateFiles(folder, "*.jpg"))
+            {
+                DateTime dateTime;
+                using (var reader = new ExifReader(file))
+                {
+                    if (!reader.GetTagValue(ExifTags.DateTimeOriginal, out dateTime)) continue;
+                }
+
+                string subFolder = Path.Combine(folder, dateTime.ToString("yyyy-MM-dd"));
+                Directory.CreateDirectory(subFolder);
+                File.Move(file, Path.Combine(subFolder, Path.GetFileName(file)));
+            }
+        }
+    }
+}
